d3d3179210a3bf6015d0972529455604441e4272
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-02 17:16:51 +0530

    Computing c+a/b

--- arithmeticComputingAndSorting.sh
+++ arithmeticComputingAndSorting.sh
@@ -1,9 +1,9 @@
-# compute a*b+c
-
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 arithmeticOp1=$((a+(b*c)))
 arithmeticOp2=$(((a*b)+c))
+arithmeticOp3=$((c+(a/b)))
 echo "ArithmeticOp1= $arithmeticOp1"
 echo "ArithmeticOp2= $arithmeticOp2"
+echo "ArithmeticOp3= $arithmeticOp3"
