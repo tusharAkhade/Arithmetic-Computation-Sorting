5b48d9ff0872b9eb28e8f60c469ec681cf120522
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-02 21:53:22 +0530

    Reading the values from dictinary into array

--- arithmeticComputingAndSorting.sh
+++ arithmeticComputingAndSorting.sh
@@ -1,13 +1,21 @@
 declare -A result
+declare -a resultArray
 read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
+
 arithmeticOp1=$((a+(b*c)))
 arithmeticOp2=$(((a*b)+c))
 arithmeticOp3=$((c+(a/b)))
 arithmeticOp4=$(((a%b)+c))
+
 result[0]=$arithmeticOp1
 result[1]=$arithmeticOp2
 result[2]=$arithmeticOp3
 result[3]=$arithmeticOp4
-echo "result: ${result[@]}"
+
+for (( i=0; i<${#result[@]}; i++ ))
+do
+	resultArray[i]=${result[$i]}
+done
+echo "resultArray: ${resultArray[@]}"
